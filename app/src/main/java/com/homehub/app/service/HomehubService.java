30f6f0b6050b7edad19657793d0e5f5221336114
abhijitayarekar <abhijitayarekar@users.noreply.github.com>
2020-09-10 13:25:22 -0700

    Updated service. Added homehub location to ui.

--- app/src/main/java/com/homehub/app/service/HomehubService.java
+++ app/src/main/java/com/homehub/app/service/HomehubService.java
@@ -11,16 +11,27 @@ import android.os.Message;
 
 import androidx.annotation.NonNull;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.net.MulticastSocket;
+import java.net.SocketTimeoutException;
+import java.net.UnknownHostException;
+import java.util.Locale;
+
 public class HomehubService extends Service {
-    public static final int BindState_UnBinded = 0;
-    public static final int BindState_Binding = 1;
-    public static final int BindState_Binded = 2;
-    public static final int BindState_UnBinding = 3;
+    public static final int Event_Connected = 1;
+
+    private static final int SSDP_PORT = 1900;
+    private static final String SSDP_ADDRESS = "239.255.255.250";
 
     private final IBinder mBinder = new HomehubBinder();
     private final HubHandler mHubHandler = new HubHandler();
     private boolean mKeepRunning = true;
     private HomehubCallback mCallback;
+    private String mHomehubLocation;
 
     @Override
     public void onDestroy() {
@@ -44,22 +55,49 @@ public class HomehubService extends Service {
         @Override
         public void run() {
             super.run();
+
+            DatagramSocket ssdpSocket = null;
             try {
-                Thread.sleep(5000);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-
-            if (mCallback != null)
-                mCallback.onHomehubConnected();
-
-            while (mKeepRunning) {
-                try {
-                    Thread.yield();
-                    Thread.sleep(1000);
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
+                ssdpSocket = new DatagramSocket();
+                ssdpSocket.setSoTimeout(5000);
+                byte[] mSearchBytes = String.format("M-SEARCH * HTTP/1.1\r\n" +
+                        "HOST: %s:%d\r\n" +
+                        "MAN: \"ssdp:discover\"\r\n" +
+                        "MX: 1\r\n" +
+                        "ST: urn:aya-home-org:service:homehub:1\r\n" +
+                        "\r\n", SSDP_ADDRESS, SSDP_PORT).getBytes();
+                DatagramPacket mSearchPacket = new DatagramPacket(mSearchBytes, 0, mSearchBytes.length, InetAddress.getByName(SSDP_ADDRESS), SSDP_PORT);
+                byte[] buf = new byte[1024];
+                DatagramPacket ssdpPacket = new DatagramPacket(buf, buf.length);
+                String homehubLocation = null;
+                while (mKeepRunning) {
+                    if (homehubLocation != null) {
+                        if (!homehubLocation.equals(mHomehubLocation) && mCallback != null) {
+                            mHomehubLocation = homehubLocation;
+                            mCallback.onHomehubEvent(Event_Connected);
+                        } else {
+                            Thread.sleep(6000);
+                            continue;
+                        }
+                    }
+                    ssdpSocket.send(mSearchPacket);
+                    try {
+                        ssdpSocket.receive(ssdpPacket);
+                    } catch (SocketTimeoutException se) {
+                        continue;
+                    }
+                    String resp = new String(ssdpPacket.getData(), 0, ssdpPacket.getLength());
+                    if (resp.startsWith("HTTP/1.1 200 OK\r\n")) {
+                        int locationStartIdx = resp.indexOf("LOCATION: ") + "LOCATION: ".length();
+                        int locationEndIdx = resp.indexOf("\r\n", locationStartIdx);
+                        homehubLocation = resp.substring(locationStartIdx, locationEndIdx);
+                    }
                 }
+            } catch (Exception e) {
+                e.printStackTrace();
+            } finally {
+                if (ssdpSocket != null && !ssdpSocket.isClosed())
+                    ssdpSocket.close();
             }
         }
     }
@@ -71,8 +109,7 @@ public class HomehubService extends Service {
     }
 
     public interface HomehubCallback {
-        void onHomehubConnected();
-        void onHomehubEvent();
+        void onHomehubEvent(int HomehubService_Event);
     }
 
     @Override
@@ -82,5 +119,8 @@ public class HomehubService extends Service {
 
     public void setCallback(HomehubCallback callBack) {
         mCallback = callBack;
+        mHomehubLocation = null;
     }
+
+    public String getLocation() { return mHomehubLocation; }
 }
--- app/src/main/java/com/homehub/app/ui/MainActivity.java
+++ app/src/main/java/com/homehub/app/ui/MainActivity.java
@@ -6,49 +6,38 @@ import android.content.Intent;
 import android.content.ServiceConnection;
 import android.os.Bundle;
 import android.os.IBinder;
+import android.widget.TextView;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.homehub.app.service.HomehubService;
 
 public class MainActivity extends AppCompatActivity implements HomehubService.HomehubCallback {
-    public static final String EXTRA_MESSAGE = "homehub.message";
-
-    private static final String S_HOMEHUB_SERVICE_STATE = "homehub.service.state";
-
     HomehubService mHomehubService;
-    int mBindState = HomehubService.BindState_UnBinded;
+    TextView mConnectionStatusView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        if (savedInstanceState != null)
-            mBindState = savedInstanceState.getInt(S_HOMEHUB_SERVICE_STATE);
-
-        if (mBindState == HomehubService.BindState_UnBinded) {
-            System.out.println("Binding homehub service.");
-            Intent intent = new Intent(this, HomehubService.class);
-            bindService(intent, connection, Context.BIND_AUTO_CREATE);
-            mBindState = HomehubService.BindState_Binding;
-        }
+        mConnectionStatusView = findViewById(R.id.textViewConnection);
     }
 
     @Override
-    protected void onSaveInstanceState(@NonNull Bundle outState) {
-        super.onSaveInstanceState(outState);
-        outState.putInt(S_HOMEHUB_SERVICE_STATE, mBindState);
+    protected void onPause() {
+        super.onPause();
+        unbindService(connection);
+        if (mHomehubService != null)
+            mHomehubService.setCallback(null);
     }
 
     @Override
-    protected void onDestroy() {
-        super.onDestroy();
-        if (mBindState > HomehubService.BindState_UnBinded)
-            unbindService(connection);
-
-        mBindState = HomehubService.BindState_UnBinded;
+    protected void onResume() {
+        super.onResume();
+        Intent intent = new Intent(this, HomehubService.class);
+        bindService(intent, connection, Context.BIND_AUTO_CREATE);
+        mConnectionStatusView.setText(R.string.looking_for_homehub);
+        mConnectionStatusView.setBackgroundColor(0xFF6D00);
     }
 
     private ServiceConnection connection = new ServiceConnection() {
@@ -57,24 +46,19 @@ public class MainActivity extends AppCompatActivity implements HomehubService.Ho
             HomehubService.HomehubBinder binder = (HomehubService.HomehubBinder) service;
             mHomehubService = binder.getService();
             mHomehubService.setCallback(MainActivity.this);
-            mBindState = HomehubService.BindState_Binded;
         }
 
         @Override
         public void onServiceDisconnected(ComponentName name) {
             mHomehubService.setCallback(null);
-            mBindState = HomehubService.BindState_UnBinded;
         }
     };
 
     @Override
-    public void onHomehubConnected() {
-        Intent intent = new Intent(this, DisplayMessageActivity.class);
-        intent.putExtra(EXTRA_MESSAGE, "Hub Connected");
-        startActivity(intent);
-    }
-
-    @Override
-    public void onHomehubEvent() {
+    public void onHomehubEvent(int event) {
+        if (event == HomehubService.Event_Connected) {
+            mConnectionStatusView.setText(mHomehubService.getLocation());
+            mConnectionStatusView.setBackgroundColor(0xFF64DD17);
+        }
     }
 }
